a9b174ae0911fd8c384ef80f8e3537b6a899c2c2
Eddie Webb <ollitech@gmail.com>
2018-12-22 20:29:25 -0500

    save last unlabeled PR and replay on label addition

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_restful import Api, Resource
 import requests
 import json
 import os
+import pickle
 
 
 # Set environment Variables
@@ -20,82 +21,111 @@ def __main__():
 	    return app.send_static_file('index.html')
 
 	# soecific API endpiunts
-	api.add_resource(WebHook,"/check_for_label/<string:tag_name>")
+	api.add_resource(WebhookResource,"/check_for_label/<string:tag_name>")
 
 	app.run(debug=True, host='0.0.0.0', port=port)
 
 
-class WebHook(Resource):
-	def forward_if_valid(self, tag_name):
-		# GitHub form-encoded data just wraps JSON in a "payload" form element.
-		payload = json.loads(request.form['payload'])
 
-		action = payload['action']
-		if ( action not in ["synchronize","opened","labeled"] ):
-			return self.send_to_circle()
 
-		print("PR Based webhook, inspecting for label")
 
-		# if our magic label was added, build it.
-		if ( action == "labeled" and payload['label']['name'] == tag_name ):
-			return self.send_to_circle()
+class WebhookResource(Resource):
 
-		# otherwise, inpect existing labels
-		allow_it = True
-		try:
-			is_forked = payload['pull_request']['head']['repo']['fork']
-			if ( is_forked ):
-				print( "Fork you!" )
-				allow_it = False
-				labels = payload['pull_request']['labels']
-				for label in labels:
-					if ( label['name'] == tag_name ):
-						allow_it = True
-						print("Found PR label with value: " + tag_name + ", will pass webhook along")
-						break				
-
-		except KeyError:
-			print("Fork key not found, pass it along")
-
-		if (allow_it):
-			return self.send_to_circle()
+	def post(self, tag_name):
+		self.webhook = Webhook(request.headers, request.form)
+		# we only filter PRs, looking for unapproved forks
+		if ( request.headers["X-GitHub-Event"] == "pull_request" ):
+			return self.forward_if_valid(tag_name)
 		else:
-			return 400
+			return self.send_to_circle(self.webhook)
+		
+	def forward_if_valid(self, tag_name):		
+		if ( self.webhook.action not in ["synchronize","opened","labeled"] ) or (not self.webhook.is_forked) :
+			return self.send_to_circle(self.webhook)
+		# if our magic label was added, send last attempt on this PR.
+		if ( self.webhook.action == "labeled" and self.webhook.payload['label']['name'] == tag_name ):
+			print("New label, attempt replay")
+			return self.send_last_to_circle()
+
+		if (self.is_label_set(tag_name)):
+			print("Contains label, pass along")
+			return self.send_to_circle(self.webhook)
+		else:
+			print("Not labled, save for future replay")
+			self.webhook.to_file()
+			return 200
 
 
-	def send_to_circle(self):
-		print("Attempting to forward webhook to CircleCI...")
-
-		#requests_session = requests.Session()
-		#
-		#  CircleCI only plays nice with application/x-www-form-urlencoded
-		#
+	def send_to_circle(self,webhook):
 		response = requests.post(
 			'https://circle.blueskygreenbuilds.com/hooks/github',
 			headers={
 				"Content-Type": "application/x-www-form-urlencoded",
-				"X-GitHub-Delivery": request.headers["X-GitHub-Delivery"],
-				"X-GitHub-Event": self.event,
-				"X-Hub-Signature": request.headers["X-Hub-Signature"],
+				"X-GitHub-Delivery": webhook.headers["X-GitHub-Delivery"],
+				"X-GitHub-Event": webhook.headers["X-GitHub-Event"],
+				"X-Hub-Signature": webhook.headers["X-Hub-Signature"],
 			},
-			data=request.form,
+			data=webhook.form,
 			verify=False
 		)
-		print("Response from CIrcleCI:")
-		print(response.status_code)
 		response.raise_for_status()
-		print("Success!")
 		return response.status_code
 
 
-	def post(self, tag_name):
-		self.event =  request.headers["X-GitHub-Event"]
-		# we only filter PRs, looking for unapproved forks
-		if ( self.event == "pull_request" ):
-			return self.forward_if_valid(tag_name)
-		else:
-			return self.send_to_circle()
-		
+
+	def is_label_set(self, label_name):
+		for label in self.webhook.labels:
+			if ( label['name'] == label_name ):
+				print("Found PR label with value: " + label_name + ", will pass webhook along")
+				return True	
+		return False
+
+	def send_last_to_circle(self):
+		try:
+			return self.send_to_circle(Webhook.from_file(self.webhook.pr_id))
+		except FileNotFoundError:
+			print("Previous webhook for newly labeled PR not found, no webhook to send.")
+			return 200
+
+class Webhook:
+	def __init__(self, headers, form):
+		self.headers = {}
+		self.headers['X-GitHub-Event'] 		= headers["X-GitHub-Event"]
+		self.headers['X-GitHub-Delivery'] 	= headers["X-GitHub-Delivery"]
+		self.headers['X-Hub-Signature'] 	= headers["X-Hub-Signature"]
+		self.form 	 						= form		
+
+		# convenience
+		self.payload = json.loads(self.form['payload'])
+		self.is_forked = self.payload['pull_request']['head']['repo']['fork']
+		self.action = self.payload['action']
+		self.pr_id = self.payload['pull_request']['id']
+		self.labels = self.payload['pull_request']['labels']
+
+
+	def to_file(self):
+		Webhook.write_file(str(self.pr_id) + '-headers.bin', self.headers)
+		Webhook.write_file(str(self.pr_id) + '-form.bin', self.form)
+
+	@staticmethod
+	def from_file(pr_id):
+		headers = Webhook.read_file(str(pr_id) + '-headers.bin')
+		form = Webhook.read_file(str(pr_id) + '-form.bin')
+		return Webhook(headers, form)
+
+	@staticmethod
+	def read_file(file_name):		
+		binary_file = open(file_name, mode='rb')
+		contents = pickle.load(binary_file)
+		binary_file.close()
+		os.remove(file_name)
+		return contents
+
+	@staticmethod
+	def write_file(file_name,contents):	
+		binary_file = open(file_name, mode='wb')
+		pickle.dump(contents, binary_file)
+		binary_file.close()
 
 
 __main__()
