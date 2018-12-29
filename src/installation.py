b2a8d1e421b66ea3a76d6c27baff6bf62c7a2a83
Eddie Webb <ollitech@gmail.com>
2018-12-28 20:47:39 -0500

    can read org repos via install token

--- src/installation.py
+++ src/installation.py
@@ -1,38 +1,45 @@
 from flask import request;
 from flask_restful import Resource
-from Crypto.PublicKey import RSA
 import github3
 import os
 import logging
+import requests
 
 class InstallationResource(Resource):
 	def post(self):		
 		payload = request.json
 		if(payload['action'] == "created"):
-
-			print("*********************")
-			print(request.headers)
 			install_id = payload['installation']['id']
 			print("New install: " + str(install_id) )
+			print("*********************")
+			print(request.headers)
 			self.authenticate_app(install_id)
 			print("*********************")
-
-			self.update_repositories()
+			self.update_webhooks()
 
 
 
 	def authenticate_app(self, install_id):
-		print("authenticating for : " + app_identifier)
 		gh = github3.GitHub()
-		#client = gh.login_as_app(private_key_pem=key_file_pem, app_id=app_identifier)
-		#print(client)
-		gh.login_as_app_installation(private_key_pem=key_file_pem, app_id=app_identifier, installation_id=install_id)
+		gh.login_as_app_installation(key_file_pem, app_identifier, install_id)
 		self.gh = gh
+		self.access_token = gh.session.auth.token
+		#print(gh.session.auth.__dict__)
 		
-	def update_repositories(self):
-		repos = self.gh.repositories()
-		for repo in repos:
-			print(repo)
+	def update_webhooks(self):
+		response = requests.get('https://api.github.com/installation/repositories',
+			headers = {
+				"Authorization" : 'token ' + self.access_token,
+				"Accept": 'application/vnd.github.machine-man-preview+json'
+			})
+	
+		response.raise_for_status()
+		repositories = response.json()['repositories']
+		for repo in repositories:
+			print(repo['name'])
+
+		
+
 
 
 webhook_secret=os.getenv("GITHUB_WEBHOOK_SECRET")
@@ -40,4 +47,3 @@ app_identifier=os.getenv("GITHUB_APP_IDENTIFIER")
 private_key_path=os.getenv("GITHUB_PRIVATE_KEY")
 key_file=open(private_key_path, "rb")
 key_file_pem=key_file.read()
-private_key=RSA.importKey(key_file_pem)
