fd38b247ba3a51b5cfd5c12983d9ced7e2f17830
Eddie Webb <ollitech@gmail.com>
2018-12-28 21:19:24 -0500

    modifies all org owned repo webhooks that use circle to point to build bully

--- src/installation.py
+++ src/installation.py
@@ -4,6 +4,7 @@ import github3
 import os
 import logging
 import requests
+import json
 
 class InstallationResource(Resource):
 	def post(self):		
@@ -37,6 +38,34 @@ class InstallationResource(Resource):
 		repositories = response.json()['repositories']
 		for repo in repositories:
 			print(repo['name'])
+			hook_r = requests.get('https://api.github.com/repos/' + repo ['full_name'] + '/hooks',
+				headers = {
+					"Authorization" : 'token ' + self.access_token,
+					"Accept": 'application/vnd.github.machine-man-preview+json'
+				})
+		
+			hook_r.raise_for_status()
+			webhooks = hook_r.json()
+			for hook in webhooks:
+				w_url=hook['config']['url']
+				if( w_url == 'https://circleci.com/hooks/github'):
+					patch = '{"config": {"url":"http://buildbully.cfapp.io/check_for_label/allow"}'
+					update = requests.patch('https://api.github.com/repos/' + repo ['full_name'] + '/hooks/' + str(hook['id']),
+						headers = {
+							"Authorization" : 'token ' + self.access_token,
+							"content-type" : "application/json"
+						},
+						data=patch)
+					print(update.text)
+					update.raise_for_status()
+					print("Bullied Circle Config")
+				elif( w_url == 'http://buildbully.cfapp.io/check_for_label/allow'):
+					print("already bullied")
+				else:
+					print("not CIrcle CI hhook")
+				
+
+
 
 		
 
