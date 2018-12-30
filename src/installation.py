d3e719eb9318489665e11f21ac77485bd8b1cdc4
Eddie Webb <ollitech@gmail.com>
2018-12-29 19:38:18 -0500

    can load users access_token from /setup, but done as deadend API, needs to be moved to web ui

--- src/installation.py
+++ src/installation.py
@@ -6,17 +6,26 @@ import logging
 import requests
 import json
 
+
+
 class InstallationResource(Resource):
+
 	def post(self):		
 		payload = request.json
-		if(payload['action'] == "created"):
+		if(request.headers["X-GitHub-Event"] == 'installation' and payload['action'] == "created"):
 			install_id = payload['installation']['id']
 			print("New install: " + str(install_id) )
 			print("*********************")
 			print(request.headers)
 			self.authenticate_app(install_id)
 			print("*********************")
-			self.update_webhooks()
+			self.update_webhooks(action="bully")
+		elif(payload['action'] == "deleted"):
+			install_id = payload['installation']['id']
+			print("Removed install: " + str(install_id) )
+
+
+		# repo added, or webhook added/chanegd
 
 
 
@@ -27,7 +36,7 @@ class InstallationResource(Resource):
 		self.access_token = gh.session.auth.token
 		#print(gh.session.auth.__dict__)
 		
-	def update_webhooks(self):
+	def update_webhooks(self, action):
 		response = requests.get('https://api.github.com/installation/repositories',
 			headers = {
 				"Authorization" : 'token ' + self.access_token,
@@ -48,26 +57,30 @@ class InstallationResource(Resource):
 			webhooks = hook_r.json()
 			for hook in webhooks:
 				w_url=hook['config']['url']
-				if( w_url == 'https://circleci.com/hooks/github'):
-					patch = '{"config": {"url":"http://buildbully.cfapp.io/check_for_label/allow"}'
-					update = requests.patch('https://api.github.com/repos/' + repo ['full_name'] + '/hooks/' + str(hook['id']),
-						headers = {
-							"Authorization" : 'token ' + self.access_token,
-							"content-type" : "application/json"
-						},
-						data=patch)
-					print(update.text)
-					update.raise_for_status()
+				if( action == 'bully' and w_url == circleci_url ):
+					patch = '{"config": {"url":"' + bully_url + '"}'
+					patch_webhook(patch)
 					print("Bullied Circle Config")
-				elif( w_url == 'http://buildbully.cfapp.io/check_for_label/allow'):
-					print("already bullied")
+				elif( action == 'release' and w_url == bully_url ):
+					patch = '{"config": {"url":"' + circleci_url + '"}'
+					patch_webhook(patch)
+					print("released repo")
 				else:
 					print("not CIrcle CI hhook")
-				
+
+	def patch_webhook(self, patch):
+		update = requests.patch('https://api.github.com/repos/' + repo ['full_name'] + '/hooks/' + str(hook['id']),
+			headers = {
+				"Authorization" : 'token ' + self.access_token,
+				"content-type" : "application/json"
+			},
+			data=patch)
+		print(update.text)
+		update.raise_for_status()
 
 
-
-		
+circleci_url='https://circleci.com/hooks/github'
+bully_url='http://buildbully.cfapp.io/check_for_label/allow'
 
 
 
--- src/webhook.py
+++ src/webhook.py
@@ -1,5 +1,5 @@
 from flask import request;
-from flask_restful import Resource
+from flask_restful import Resource,reqparse
 
 
 class WebhookResource(Resource):
